b9e224b8abcfa89cc7eec0c67bba4a7a7d96e4f3
DESKTOP-65L9TO1\lvjiajia <lvjiajia@pwrd.com>
2022-11-24 20:51:26 +0800

    init

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require("http")
 
 const users = [
-    { id: 11111, name: "zhangsan" },
-    { id: 22222, name: "lisi" },
-    { id: 33333, name: "wangwu" },
+    { id: 333, name: "zhangsan" },
+    { id: 444, name: "lisi" },
+    { id: 555, name: "wangwu" },
 ]
 
 const server = http.createServer((req, res) => {
