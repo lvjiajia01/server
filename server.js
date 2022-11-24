ad02aa3fd8ee576337478a64ceb2aec177056e25
DESKTOP-65L9TO1\lvjiajia <lvjiajia@pwrd.com>
2022-11-24 23:29:40 +0800

    init

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ const server = http.createServer((req, res) => {
     }
 })
 
-server.listen(3000, () => {
-    console.log("server is running at port 3000!")
+server.listen(3001, () => {
+    console.log("server is running at port 3001!")
 })
